39d45023dfeaf5c9bf068565b601c8f645abdd38
robcs9 <rcs9@protonmail.com>
2023-11-03 10:03:10 -0300

    Integração inicial do cadastro de Usuários

--- src/components/Login/Register/Register.jsx
+++ src/components/Login/Register/Register.jsx
@@ -1,35 +1,5 @@
 import { useState } from "react";
-
-const fields = [
-  {
-    label: "NOME",
-    type: "text",
-    placeholder: "Digite o seu nome",
-    required: true,
-  },
-  { label: "CEP", type: "text", placeholder: "Digite o seu cep" },
-  { label: "CIDADE", type: "text", placeholder: "", disabled: true },
-  { label: "BAIRRO", type: "text", placeholder: "", disabled: true },
-  {
-    label: "EMAIL",
-    type: "email",
-    placeholder: "Digite o seu email",
-    required: true,
-  },
-  { label: "CPF", type: "text", placeholder: "***.***.**-**", required: true },
-  {
-    label: "SENHA",
-    type: "password",
-    placeholder: "Crie uma senha",
-    required: true,
-  },
-  {
-    label: "CONFIRME SUA SENHA",
-    type: "password",
-    placeholder: "Confirme sua senha",
-    required: true,
-  },
-];
+import axios from "axios";
 
 const Register = () => {
   const [nome, setNome] = useState();
@@ -41,6 +11,96 @@ const Register = () => {
   const [senha, setSenha] = useState();
   const [confirmaSenha, setConfirmaSenha] = useState();
 
+  const fields = [
+    {
+      label: "NOME",
+      type: "text",
+      placeholder: "Digite o seu nome",
+      required: true,
+      handleChange: (e) => setNome(e.target.value),
+    },
+    {
+      label: "CEP",
+      type: "text",
+      placeholder: "Digite o seu cep",
+      handleChange: (e) => setCep(e.target.value),
+    },
+    {
+      label: "CIDADE",
+      type: "text",
+      placeholder: "",
+      disabled: true,
+      handleChange: (e) => setCidade(e.target.value),
+    },
+    {
+      label: "BAIRRO",
+      type: "text",
+      placeholder: "",
+      disabled: true,
+      handleChange: (e) => setBairro(e.target.value),
+    },
+    {
+      label: "EMAIL",
+      type: "email",
+      placeholder: "Digite o seu email",
+      required: true,
+      handleChange: (e) => setEmail(e.target.value),
+    },
+    {
+      label: "CPF",
+      type: "text",
+      placeholder: "***.***.**-**",
+      required: true,
+      handleChange: (e) => setCpf(e.target.value),
+    },
+    {
+      label: "SENHA",
+      type: "password",
+      placeholder: "Crie uma senha",
+      required: true,
+      handleChange: (e) => setSenha(e.target.value),
+    },
+    {
+      label: "CONFIRME SUA SENHA",
+      type: "password",
+      placeholder: "Confirme sua senha",
+      required: true,
+      handleChange: (e) => setConfirmaSenha(e.target.value),
+    },
+  ];
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    salvar();
+  }
+
+  function salvar() {
+    const usuarioRequest = {
+      nome: nome,
+      cep: cep,
+      cidade: cidade,
+      bairro: bairro,
+      email: email,
+      cpf: cpf,
+      senha: senha,
+      confirmaSenha: confirmaSenha,
+    };
+    if (senha != confirmaSenha) {
+      alert("Senha e Confirmação não conferem.");
+    } else {
+      axios
+        .post("http://localhost:8082/api/usuario", usuarioRequest)
+        .then(
+          //(res) => console.log("Usuário cadastrado com sucesso.")
+          (r) => alert("cadastrado " + r.data)
+        )
+        .catch(
+          //(err) => console.log("Erro ao cadastrar usuário.")
+          (e) => alert("falha ao cadastrar.\n" + e.name + " - " + e.message)
+        );
+    }
+  }
+
   return (
     <main className="flex container justify-center mx-auto mb-8">
       <div>
@@ -50,7 +110,10 @@ const Register = () => {
         <p className="text-slate-500 w-[230px] text-[18px]">
           Mantenha-se informado sobre a sua região.
         </p>
-        <form className="mt-[12px] border-2 rounded-lg bg-white pl-[46px] pr-[38px] pt-[45px] w-[540px] shadow py-[32px]">
+        <form
+          className="mt-[12px] border-2 rounded-lg bg-white pl-[46px] pr-[38px] pt-[45px] w-[540px] shadow py-[32px]"
+          onSubmit={handleSubmit}
+        >
           {fields.map((field, index) => (
             <div key={index}>
               <label
@@ -65,12 +128,13 @@ const Register = () => {
                 required={field.required}
                 type={field.type}
                 placeholder={field.placeholder}
+                onChange={field.handleChange}
                 className="border rounded-[6px] p-3 w-full mb-4 text-black"
               />
             </div>
           ))}
           <div className="flex gap-2">
-            <input id="termos" type="checkbox" placeholder="oii" />
+            <input id="termos" type="checkbox" required />
             <label htmlFor="termos" className="text-black">
               Eu concordo com os{" "}
               <a href="" className="text-blue-400 underline">
@@ -78,7 +142,10 @@ const Register = () => {
               </a>
             </label>
           </div>
-          <button className="font-bold bg-blue-400 px-[52px] py-[12px] rounded-lg mt-3 hover:bg-red-600">
+          <button
+            type="submit"
+            className="font-bold bg-blue-400 px-[52px] py-[12px] rounded-lg mt-3 hover:bg-red-600"
+          >
             CADASTRAR
           </button>
         </form>
