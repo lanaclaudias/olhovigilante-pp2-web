b53d4d4eb53249d070ed0387b02daa532da09b0c
mary0077 <marianagomes201@hotmail.com>
2023-11-16 17:35:54 -0300

    Modificações

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,14 @@
 import logo from "/mini-logo.png";
 
+const icons = {
+  home: "/icon-inicio.svg",
+  ocorrencias: '/icon-ocorrencias.svg',
+  comunidade: '/icon-comunidade.svg',
+  ondeDenunciar: '/megaphone.svg',
+  perfil: '/icon-usuario.svg'
+}
+//const activeIcon = isLoggedIn ? "active" : null;
+
 const Header = () => {
   return (
     <header className="bg-black">
@@ -11,14 +20,14 @@ const Header = () => {
           <ul className="flex items-center gap-12">
             <li>
               <a href="/" className="flex flex-col">
-                <img src="icon-inicio.svg" alt="" className="self-center" />
+                <img src={icons.home} alt="" className="self-center" />
                 <p>Início</p>
               </a>
             </li>
             <li>
               <a href="/ocorrencia" className="flex flex-col">
                 <img
-                  src="icon-ocorrencias.svg"
+                  src={icons.ocorrencias}
                   alt=""
                   className="self-center"
                 />
@@ -27,20 +36,20 @@ const Header = () => {
             </li>
             <li>
               <a href="comunidade" className="flex flex-col">
-                <img src="icon-comunidade.svg" alt="" className="self-center" />
+                <img src={icons.comunidade} alt="" className="self-center" />
                 <p>Comunidade</p>
               </a>
             </li>
             <li>
               <a href="/denunciar" className="flex flex-col">
-                <img src="megaphone.svg" alt="" className="self-center w-8" />
+                <img src={icons.ondeDenunciar} alt="" className="self-center w-8" />
                 <p>Onde Denunciar</p>
               </a>
             </li>
           </ul>
         </nav>
         <a href="/perfil" className="flex flex-col">
-          <img src="icon-usuario.svg" alt="" className="self-center w-8" />
+          <img src={icons.perfil} alt="" className="self-center w-8" />
           <p>Perfil</p>
         </a>
       </div>
