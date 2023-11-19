26787fbc6f53bd0301de33edc9f82db7c03bb673
mary0077 <marianagomes201@hotmail.com>
2023-11-19 19:24:12 -0300

    iniciação da tela home

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -11,12 +11,12 @@ function Rotas() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
         {/*<Route path="sobre" element={ <Sobre/> } />*/}
         {/*<Route path="contato" element={ <Contato/> } />*/}
         <Route path="login" element={<Login />} />
         <Route path="cadastro" element={<Cadastro />} />
         <Route path="ocorrencia" element={<Ocorrencia />} />
+        <Route path="home" element={<Home />} />
         <Route path="*" element={<NotFound />} /> {/* Página 404 */}
       </Routes>
     </>
