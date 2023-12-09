cd6ed31cd1348f483611fb12fc317092d74835f2
mary0077 <marianagomes201@hotmail.com>
2023-12-08 21:12:16 -0300

    ajustes na tela inicial de acordo com o protótipo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,13 +9,13 @@ const Home = () => {
         {/* Barra de Navegação */}
         <div className="">
           {/* Banner */}
-          <div className="relative">
-
-        <div className={`bg-[url(/olho.png)] w-full bg-cover flex items-center bg-no-repeat h-[800px]`} >
-        <div className='container'>
+        <div className={`bg-[url(/olho.png)] w-full bg-cover flex items-center bg-no-repeat h-[700px]`} >
+        <div className="container">
+          
+        <div className='container absolute inset-y-12 left-20'>
             
-            <div className="container">
-              <p className="text-black w-[590px] text-left text-[35px] font-bold">
+            <div className="container absolute inset-y-20 left-20">
+              <p className="text-black w-[750px] text-[35px] font-bold pt-[30px] pb-5">
               No Olho Vigilante, proporcionamos uma experiência intuitiva e eficiente para conduzir pesquisas relacionadas a incidentes, estatísticas e informações relevantes. Nossa plataforma robusta permite que os usuários realizem pesquisas abrangentes, filtrando dados específicos e obtendo insights valiosos para melhor compreensão e consciência da segurança em suas comunidades.
               </p>
 
@@ -25,12 +25,12 @@ const Home = () => {
 
             </div>
           </div>
-
+        
   </div>
  </div>
 </div>
 
-<div className={`bg-black pt-[150px] pb-1`} >
+<div className={`bg-black pt-[70px] pb-1`} >
 
 <div className="container">
               <p className="text-white w-[599px] text-left text-[35px] font-bold pb-4">
@@ -40,7 +40,7 @@ const Home = () => {
               Cada uma das opções abaixo abre um leque de serviços para sua necessidade.
               </p>
 
-<div className='flex gap-1'>
+<div className='flex gap-1 h-[200px]'>
 
 <button className="bg-white text-black font-bold py-2 px-4 rounded w-[200px] h-[116px]">
               PESQUISAR
@@ -68,13 +68,11 @@ const Home = () => {
 </div>
 
 
-
 </div>
-
-
       
 </>
 );
 };
 
 export default Home;
+
